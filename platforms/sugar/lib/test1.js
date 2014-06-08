871ebd60d15975e1c43b750c02e690f44846e4f6
Puneet Kaur <puneet.gkaur@gmail.com>
2014-06-08 13:36:34 +0530

    initial commit

---
+++ platforms/sugar/lib/test1.js
@@ -0,0 +1,22 @@
+var fs=require('fs');
+
+var dir='./sugar-web';
+var data={};
+
+fs.readdir(dir,function(err,files){
+    if (err) throw err;
+//    var c=0;
+    files.forEach(function(file){
+/*        c++;
+        fs.readFile(dir+file,'utf-8',function(err,html){
+            if (err) throw err;
+            data[file]=html;
+            if (0===--c) {
+                console.log(data);  //socket.emit('init', {data: data});
+            }
+*/
+	console.log(file);
+
+//        });
+    });
+});
